f9dbc9d4b544367ebcd8b5d388f2710e4b4eed10
Juan Pablo <jballes89830@universidadean.edu.co>
2021-11-02 17:52:29 -0500

    estructura hecha

---
+++ app.js
@@ -0,0 +1,17 @@
+const aside = document.getElementById("aside");
+const topAside = aside.offsetTop;
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY > topAside) {
+        aside.style.position = "fixed";
+    } else {
+        aside.style.position = "relative";
+        aside.style.alignItems = "flex-start";
+    }
+
+    if (window.scrollY > 9330) {
+        aside.style.position = "relative";
+        aside.style.alignItems = "flex-end";
+    }
+    console.log(window.scrollY)
+});
