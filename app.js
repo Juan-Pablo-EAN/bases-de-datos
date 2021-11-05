b83ad254c94d56784ac7d6415f35e6705e2cb235
Juan Pablo <jballes89830@universidadean.edu.co>
2021-11-04 21:12:30 -0500

    cambios en la animacion

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 const aside = document.getElementById("aside");
+const articulo = document.querySelector(".articulo");
+const criterios = document.getElementById("criterios");
+const footer = document.querySelector(".ft");
 const topAside = aside.offsetTop;
 
 window.addEventListener("scroll", () => {
     if (window.scrollY > topAside) {
         aside.style.position = "fixed";
+        aside.style.top = "0px";
     } else {
         aside.style.position = "relative";
         aside.style.alignItems = "flex-start";
     }
 
-    if (window.scrollY > 9330) {
+    if (window.scrollY > (footer.offsetTop - window.screen.height + 50)) { //50 es el margin bottom que tiene aside
         aside.style.position = "relative";
         aside.style.alignItems = "flex-end";
     }
